741adc5e3b2ed1c43412695d960ed7909ba022d5
dprensa <dprensa@CDE-DTI-41>
2016-12-10 14:36:05 -0400

    Se cambio template para Dashboard

--- AdminTec.MVC/App_Start/BundleConfig.cs
+++ AdminTec.MVC/App_Start/BundleConfig.cs
@@ -9,51 +9,12 @@ namespace AdminTec
         {
             #region JAVASCRIPT'S
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-3.1.1.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.min.js",
-                "~/Scripts/respond.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                "~/Scripts/vendor.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-toolkit").Include(
-                "~/Scripts/bootstrap-toolkit.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/dropzone").Include(
-                "~/Scripts/dropzone.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/eventemitter2").Include(
-                "~/Scripts/eventemitter2.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jQuerySortable").Include(
-                        "~/Scripts/jQuerySortable.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/metisMenu").Include(
-                "~/Scripts/metisMenu.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/morris").Include(
-                "~/Scripts/morris.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/mocha").Include(
-                "~/Scripts/mocha.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/raphael").Include(
-                "~/Scripts/raphael.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/ResponsiveBootstrapToolkit").Include(
-                "~/Scripts/ResponsiveBootstrapToolkit.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/Sortable").Include(
-                "~/Scripts/Sortable.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/TinyColor").Include(
-                "~/Scripts/TinyColor.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate.min.js",
-                "~/Scripts/jquery.validate.unobtrusive.min.js",
-                "~/Scripts/jquery.unobtrusive-ajax.min.js"
-                ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -64,28 +25,35 @@ namespace AdminTec
 
             #region ESTILOS
 
-            bundles.Add(new StyleBundle("~/Content/modernizr").Include(
-                      "~/Content/modernizr.css"));
+            bundles.Add(new StyleBundle("~/Content/app").Include(
+                      "~/Content/app.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/bootstrap-*.min.css"
-                      ));
+            bundles.Add(new StyleBundle("~/Content/vendor").Include(
+                      "~/Content/vendor.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/animate").Include(
-                      "~/Content/animate.min.css"));
+            bundles.Add(new StyleBundle("~/Content/app-blue").Include(
+                      "~/Content/app-blue.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
-                      "~/Content/font-awesome.min.css"));
+            bundles.Add(new StyleBundle("~/Content/app-custom").Include(
+                      "~/Content/app-custom.min.css"));
 
-            bundles.Add(new StyleBundle("~/Content/QuillEditor").Include(
-                      "~/Content/QuillEditor.css"));
+            bundles.Add(new StyleBundle("~/Content/app-green").Include(
+                      "~/Content/app-green.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/app-orange").Include(
+                      "~/Content/app-orange.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/app-purple").Include(
+                      "~/Content/app-purple.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/app-red").Include(
+                      "~/Content/app-red.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/app-seagreen").Include(
+                      "~/Content/app-seagreen.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/Site").Include(
                       "~/Content/Site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/site").Include(
-                      "~/Content/site.css"));
 
             #endregion ESTILOS
         }
