0b450822b926b12e25e23deb8bfa645e66d2dc22
dprensa <dprensa@CDE-DTI-41>
2016-12-12 21:03:26 -0400

    Se personalizo el Dashboard

--- AdminTec.MVC/Scripts/dashboard.js
+++ AdminTec.MVC/Scripts/dashboard.js
@@ -6,205 +6,319 @@
  **/
 
 $(function () {
-
-  "use strict";
-
-  //Make the dashboard widgets sortable Using jquery UI
-  $(".connectedSortable").sortable({
-    placeholder: "sort-highlight",
-    connectWith: ".connectedSortable",
-    handle: ".box-header, .nav-tabs",
-    forcePlaceholderSize: true,
-    zIndex: 999999
-  });
-  $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
-
-  //jQuery UI sortable for the todo list
-  $(".todo-list").sortable({
-    placeholder: "sort-highlight",
-    handle: ".handle",
-    forcePlaceholderSize: true,
-    zIndex: 999999
-  });
-
-  //bootstrap WYSIHTML5 - text editor
-  $(".textarea").wysihtml5();
-
-  $('.daterange').daterangepicker({
-    ranges: {
-      'Today': [moment(), moment()],
-      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month': [moment().startOf('month'), moment().endOf('month')],
-      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-    },
-    startDate: moment().subtract(29, 'days'),
-    endDate: moment()
-  }, function (start, end) {
-    window.alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-  });
-
-  /* jQueryKnob */
-  $(".knob").knob();
-
-  //jvectormap data
-  var visitorsData = {
-    "US": 398, //USA
-    "SA": 400, //Saudi Arabia
-    "CA": 1000, //Canada
-    "DE": 500, //Germany
-    "FR": 760, //France
-    "CN": 300, //China
-    "AU": 700, //Australia
-    "BR": 600, //Brazil
-    "IN": 800, //India
-    "GB": 320, //Great Britain
-    "RU": 3000 //Russia
-  };
-  //World map by jvectormap
-  $('#world-map').vectorMap({
-    map: 'world_mill_en',
-    backgroundColor: "transparent",
-    regionStyle: {
-      initial: {
-        fill: '#e4e4e4',
-        "fill-opacity": 1,
-        stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
+    //-------------
+    //- PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+    var pieChart = new Chart(pieChartCanvas);
+    var PieData = [
+      {
+          value: 700,
+          color: "#f56954",
+          highlight: "#f56954",
+          label: "Entrenamiento Usuarios"
+      },
+      {
+          value: 500,
+          color: "#00a65a",
+          highlight: "#00a65a",
+          label: "IE"
+      },
+      {
+          value: 400,
+          color: "#f39c12",
+          highlight: "#f39c12",
+          label: "null"
+      },
+      {
+          value: 600,
+          color: "#00c0ef",
+          highlight: "#00c0ef",
+          label: "Atasco"
+      },
+      {
+          value: 300,
+          color: "#3c8dbc",
+          highlight: "#3c8dbc",
+          label: "null"
+      },
+      {
+          value: 100,
+          color: "#d2d6de",
+          highlight: "#d2d6de",
+          label: "null"
       }
-    },
-    series: {
-      regions: [{
-        values: visitorsData,
-        scale: ["#92c1dc", "#ebf4f9"],
-        normalizeFunction: 'polynomial'
-      }]
-    },
-    onRegionLabelShow: function (e, el, code) {
-      if (typeof visitorsData[code] != "undefined")
-        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
-    }
-  });
+    ];
+    var pieOptions = {
+        //Boolean - Whether we should show a stroke on each segment
+        segmentShowStroke: true,
+        //String - The colour of each segment stroke
+        segmentStrokeColor: "#fff",
+        //Number - The width of each segment stroke
+        segmentStrokeWidth: 1,
+        //Number - The percentage of the chart that we cut out of the middle
+        percentageInnerCutout: 50, // This is 0 for Pie charts
+        //Number - Amount of animation steps
+        animationSteps: 100,
+        //String - Animation easing effect
+        animationEasing: "easeOutBounce",
+        //Boolean - Whether we animate the rotation of the Doughnut
+        animateRotate: true,
+        //Boolean - Whether we animate scaling the Doughnut from the centre
+        animateScale: false,
+        //Boolean - whether to make the chart responsive to window resizing
+        responsive: true,
+        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+        maintainAspectRatio: false,
+        //String - A legend template
+        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+        //String - A tooltip template
+        tooltipTemplate: "<%=value %> <%=label%> users"
+    };
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    pieChart.Doughnut(PieData, pieOptions);
+    //-----------------
+    //- END PIE CHART -
+    //-----------------
 
-  //Sparkline charts
-  var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
-  $('#sparkline-1').sparkline(myvalues, {
-    type: 'line',
-    lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
-    height: '50',
-    width: '80'
-  });
-  myvalues = [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921];
-  $('#sparkline-2').sparkline(myvalues, {
-    type: 'line',
-    lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
-    height: '50',
-    width: '80'
-  });
-  myvalues = [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21];
-  $('#sparkline-3').sparkline(myvalues, {
-    type: 'line',
-    lineColor: '#92c1dc',
-    fillColor: "#ebf4f9",
-    height: '50',
-    width: '80'
-  });
+    "use strict";
 
-  //The Calender
-  $("#calendar").datepicker();
+    //Make the dashboard widgets sortable Using jquery UI
+    $(".connectedSortable").sortable({
+        placeholder: "sort-highlight",
+        connectWith: ".connectedSortable",
+        handle: ".box-header, .nav-tabs",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    });
+    $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
 
-  //SLIMSCROLL FOR CHAT WIDGET
-  $('#chat-box').slimScroll({
-    height: '250px'
-  });
+    //jQuery UI sortable for the todo list
+    $(".todo-list").sortable({
+        placeholder: "sort-highlight",
+        handle: ".handle",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    });
 
-  /* Morris.js Charts */
-  // Sales chart
-  var area = new Morris.Area({
-    element: 'revenue-chart',
-    resize: true,
-    data: [
-      {y: '2011 Q1', item1: 2666, item2: 2666},
-      {y: '2011 Q2', item1: 2778, item2: 2294},
-      {y: '2011 Q3', item1: 4912, item2: 1969},
-      {y: '2011 Q4', item1: 3767, item2: 3597},
-      {y: '2012 Q1', item1: 6810, item2: 1914},
-      {y: '2012 Q2', item1: 5670, item2: 4293},
-      {y: '2012 Q3', item1: 4820, item2: 3795},
-      {y: '2012 Q4', item1: 15073, item2: 5967},
-      {y: '2013 Q1', item1: 10687, item2: 4460},
-      {y: '2013 Q2', item1: 8432, item2: 5713}
-    ],
-    xkey: 'y',
-    ykeys: ['item1', 'item2'],
-    labels: ['Item 1', 'Item 2'],
-    lineColors: ['#a0d0e0', '#3c8dbc'],
-    hideHover: 'auto'
-  });
-  var line = new Morris.Line({
-    element: 'line-chart',
-    resize: true,
-    data: [
-      {y: '2011 Q1', item1: 2666},
-      {y: '2011 Q2', item1: 2778},
-      {y: '2011 Q3', item1: 4912},
-      {y: '2011 Q4', item1: 3767},
-      {y: '2012 Q1', item1: 6810},
-      {y: '2012 Q2', item1: 5670},
-      {y: '2012 Q3', item1: 4820},
-      {y: '2012 Q4', item1: 15073},
-      {y: '2013 Q1', item1: 10687},
-      {y: '2013 Q2', item1: 8432}
-    ],
-    xkey: 'y',
-    ykeys: ['item1'],
-    labels: ['Item 1'],
-    lineColors: ['#efefef'],
-    lineWidth: 2,
-    hideHover: 'auto',
-    gridTextColor: "#fff",
-    gridStrokeWidth: 0.4,
-    pointSize: 4,
-    pointStrokeColors: ["#efefef"],
-    gridLineColor: "#efefef",
-    gridTextFamily: "Open Sans",
-    gridTextSize: 10
-  });
+    //bootstrap WYSIHTML5 - text editor
+    $(".textarea").wysihtml5();
 
-  //Donut Chart
-  var donut = new Morris.Donut({
-    element: 'sales-chart',
-    resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
-    data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
-    ],
-    hideHover: 'auto'
-  });
+    $('.daterange').daterangepicker({
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment()
+    }, function (start, end) {
+        window.alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    });
 
-  //Fix for charts under tabs
-  $('.box ul.nav a').on('shown.bs.tab', function () {
-    area.redraw();
-    donut.redraw();
-    line.redraw();
-  });
+    /* jQueryKnob */
+    $(".knob").knob();
 
-  /* The todo list plugin */
-  $(".todo-list").todolist({
-    onCheck: function (ele) {
-      window.console.log("The element has been checked");
-      return ele;
-    },
-    onUncheck: function (ele) {
-      window.console.log("The element has been unchecked");
-      return ele;
-    }
-  });
+    //jvectormap data
+    var visitorsData = {
+        "US": 398, //USA
+        "SA": 400, //Saudi Arabia
+        "CA": 1000, //Canada
+        "DE": 500, //Germany
+        "FR": 760, //France
+        "CN": 300, //China
+        "AU": 700, //Australia
+        "BR": 600, //Brazil
+        "IN": 800, //India
+        "GB": 320, //Great Britain
+        "RU": 3000 //Russia
+    };
+    //World map by jvectormap
+    $('#world-map').vectorMap({
+        map: 'world_mill_en',
+        backgroundColor: "transparent",
+        regionStyle: {
+            initial: {
+                fill: '#e4e4e4',
+                "fill-opacity": 1,
+                stroke: 'none',
+                "stroke-width": 0,
+                "stroke-opacity": 1
+            }
+        },
+        series: {
+            regions: [{
+                values: visitorsData,
+                scale: ["#92c1dc", "#ebf4f9"],
+                normalizeFunction: 'polynomial'
+            }]
+        },
+        onRegionLabelShow: function (e, el, code) {
+            if (typeof visitorsData[code] != "undefined")
+                el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+        }
+    });
 
+    //Sparkline charts
+    var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
+    $('#sparkline-1').sparkline(myvalues, {
+        type: 'line',
+        lineColor: '#92c1dc',
+        fillColor: "#ebf4f9",
+        height: '50',
+        width: '80'
+    });
+    myvalues = [515, 519, 520, 522, 652, 810, 370, 627, 319, 630, 921];
+    $('#sparkline-2').sparkline(myvalues, {
+        type: 'line',
+        lineColor: '#92c1dc',
+        fillColor: "#ebf4f9",
+        height: '50',
+        width: '80'
+    });
+    myvalues = [15, 19, 20, 22, 33, 27, 31, 27, 19, 30, 21];
+    $('#sparkline-3').sparkline(myvalues, {
+        type: 'line',
+        lineColor: '#92c1dc',
+        fillColor: "#ebf4f9",
+        height: '50',
+        width: '80'
+    });
+
+    //The Calender
+    $("#calendar").datepicker();
+
+    //SLIMSCROLL FOR CHAT WIDGET
+    $('#chat-box').slimScroll({
+        height: '250px'
+    });
+
+    /* Morris.js Charts */
+    // Sales chart
+    var chartColor = new Morris.Bar({
+        element: 'revenue-chart',
+        resize: true,
+        data: [
+          {
+              y: 'Septiembre',
+              copias: 148488,
+              impresiones: 135998,
+              copiasBN: 1036695,
+              impresionesBN: 802675
+          },
+          {
+              y: 'Octubre',
+              copias: 163928,
+              impresiones: 149684,
+              copiasBN: 1207612,
+              impresionesBN: 868957
+          },
+          {
+              y: 'Noviembre',
+              copias: 164770,
+              impresiones: 149870,
+              copiasBN: 1120788,
+              impresionesBN: 868271
+          },
+          {
+              y: 'Diciembre',
+              copias: 181895,
+              impresiones: 163928,
+              copiasBN: 1121698,
+              impresionesBN: 935925
+          }
+        ],
+        xkey: 'y',
+        ykeys: ['copias', 'impresiones', 'copiasBN', 'impresionesBN'],
+        labels: ['Copias Color', 'Impresiones Color', 'Copias BN', 'Impresiones BN'],
+        barColors: ['#00a65a', '#ff5722', '#4289f4', '#e8dc06'],
+        hideHover: 'auto'
+    });
+
+    var line = new Morris.Line({
+        element: 'line-chart',
+        resize: true,
+        data: [
+          { y: '2011 Q1', item1: 2666 },
+          { y: '2011 Q2', item1: 2778 },
+          { y: '2011 Q3', item1: 4912 },
+          { y: '2011 Q4', item1: 3767 },
+          { y: '2012 Q1', item1: 6810 },
+          { y: '2012 Q2', item1: 5670 },
+          { y: '2012 Q3', item1: 4820 },
+          { y: '2012 Q4', item1: 15073 },
+          { y: '2013 Q1', item1: 10687 },
+          { y: '2013 Q2', item1: 8432 }
+        ],
+        xkey: 'y',
+        ykeys: ['item1'],
+        labels: ['Item 1'],
+        lineColors: ['#efefef'],
+        lineWidth: 2,
+        hideHover: 'auto',
+        gridTextColor: "#fff",
+        gridStrokeWidth: 0.4,
+        pointSize: 4,
+        pointStrokeColors: ["#efefef"],
+        gridLineColor: "#efefef",
+        gridTextFamily: "Open Sans",
+        gridTextSize: 10
+    });
+
+    //Donut Chart
+    var chartBnW = new Morris.Bar({
+        element: 'sales-chart',
+        resize: true,
+        data: [
+          {
+              y: 'Septiembre',
+              copiasBN: 1036695,
+              impresionesBN: 802675
+          },
+          {
+              y: 'Octubre',
+              copiasBN: 1120788,
+              impresionesBN: 868271
+          },
+          {
+              y: 'Noviembre',
+              copiasBN: 1207612,
+              impresionesBN: 935925
+          },
+          {
+              y: 'Diciembre',
+              copiasBN: 1121698,
+              impresionesBN: 868957
+          }
+        ],
+        xkey: 'y',
+        ykeys: ['copiasBN', 'impresionesBN'],
+        labels: ['Copias', 'Impresiones'],
+        barColors: ['#00a65a', '#ff5722', '#4289f4', '#e8dc06'],
+        hideHover: 'auto'
+    });
+
+    //Fix for charts under tabs
+    $('.box ul.nav a').on('shown.bs.tab', function () {
+        chartColor.redraw();
+        chartBnW.redraw();
+        line.redraw();
+    });
+
+    /* The todo list plugin */
+    $(".todo-list").todolist({
+        onCheck: function (ele) {
+            window.console.log("The element has been checked");
+            return ele;
+        },
+        onUncheck: function (ele) {
+            window.console.log("The element has been unchecked");
+            return ele;
+        }
+    });
 });
